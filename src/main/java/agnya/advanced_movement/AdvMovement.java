b15573ceb806753468eb5cf590bf7178698b843a
nvb-uy <almhak@proton.me>
2022-08-16 08:25:36 -0700

    -+ Fix item

--- src/main/java/agnya/advanced_movement/AdvMovement.java
+++ src/main/java/agnya/advanced_movement/AdvMovement.java
@@ -5,11 +5,9 @@ import org.slf4j.LoggerFactory;
 
 import agnya.advanced_movement.config.AMConfig;
 import agnya.advanced_movement.speedometer.Speedometer;
-import agnya.advanced_movement.speedometer.SpeedometerTab;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -18,7 +16,7 @@ public class AdvMovement implements ModInitializer {
 	public static final String MOD_ID = "advanced_movement";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	
-	public static final Item SPEEDOMETER = new Speedometer(new FabricItemSettings().group(SpeedometerTab.ADVMOVEMENT));
+	public static final Item SPEEDOMETER = new Speedometer();
 	
 	@Override
 	public void onInitialize() {
--- src/main/java/agnya/advanced_movement/speedometer/Speedometer.java
+++ src/main/java/agnya/advanced_movement/speedometer/Speedometer.java
@@ -3,7 +3,10 @@ package agnya.advanced_movement.speedometer;
 import net.minecraft.item.Item;
 
 public class Speedometer extends Item {
-    public Speedometer(Settings settings) {
-        super(settings);
+    public Speedometer() {
+        super(new Item.Settings()
+        .group(SpeedometerTab.ADVMOVEMENT)
+        .maxCount(1)
+        );
     }
 }
