37a89a59cc423f07b543f7affd9c8c44fd12c076
nvb-uy <almhak@proton.me>
2022-08-16 07:30:19 -0700

    add itemgroup and speedometer

---
+++ src/main/java/agnya/advanced_movement/speedometer/Speedometer.java
@@ -0,0 +1,9 @@
+package agnya.advanced_movement.speedometer;
+
+import net.minecraft.item.Item;
+
+public class Speedometer extends Item {
+    public Speedometer(Settings settings) {
+        super(settings);
+    }
+}
