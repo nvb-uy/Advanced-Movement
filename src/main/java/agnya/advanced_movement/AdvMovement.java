b9353ad8a5b1dc68ea3ea7afcba3e928028820bf
nvb-uy <almhak@proton.me>
2022-08-19 09:32:43 -0700

    + Add placeholder registry for blocks

--- src/main/java/agnya/advanced_movement/AdvMovement.java
+++ src/main/java/agnya/advanced_movement/AdvMovement.java
@@ -5,9 +5,13 @@ import org.slf4j.LoggerFactory;
 
 import agnya.advanced_movement.config.AMConfig;
 import agnya.advanced_movement.speedometer.Speedometer;
+import agnya.advanced_movement.utilities.BhoppingBlock;
+import agnya.advanced_movement.utilities.BhoppingItem;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -18,11 +22,17 @@ public class AdvMovement implements ModInitializer {
 	
 	public static final Item SPEEDOMETER = new Speedometer();
 	
+	public static final Block BHOPPING_BLOCK = new BhoppingBlock();
+	public static final BlockItem BHOPPING_BLOCK_ITEM = new BhoppingItem(BHOPPING_BLOCK, null);
+	
 	@Override
 	public void onInitialize() {
 		AutoConfig.register(AMConfig.class, GsonConfigSerializer::new);
 		LOGGER.info("Advanced Movement Loaded Sucessfully!");
 
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "speedometer"), SPEEDOMETER);
+
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "bunnyhop_block"), BHOPPING_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "bunnyhop_block"), BHOPPING_BLOCK_ITEM);
 	}
 }
