c48bdca870aa586e12039f715e91107867182e42
nvb-uy <almhak@proton.me>
2022-08-19 16:39:36 -0700

    + Single 1.18-1.19 Compatibility

--- src/main/java/agnya/advanced_movement/config/Menu.java
+++ src/main/java/agnya/advanced_movement/config/Menu.java
@@ -10,7 +10,5 @@ public class Menu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> AutoConfig.getConfigScreen(AMConfig.class, parent).get();
-
     }
-
 }
