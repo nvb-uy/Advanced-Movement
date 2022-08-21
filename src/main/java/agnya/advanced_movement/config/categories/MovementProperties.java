ded0504598442799809ee4ae55a0747715fd10ee
nvb-uy <almhak@proton.me>
2022-08-21 15:02:03 -0700

    -+ Moved config into categories

---
+++ src/main/java/agnya/advanced_movement/config/categories/MovementProperties.java
@@ -0,0 +1,9 @@
+package agnya.advanced_movement.config.categories;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = "movementproperties")
+public class MovementProperties implements ConfigData {
+
+}
